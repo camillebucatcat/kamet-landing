8f8458e103053b4175ddccde8e253d2e6385a17d
hex0301 <johnhex71@gmail.com>
2023-07-10 23:28:26 +0800

    Country-Code

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { KmtService } from '../services/kmt.service';
 import { FormControl, FormGroup } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
@@ -12,6 +12,20 @@ import { TranslateManagerService } from '../services/translate-manager.service';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+  public selectedcountry: any = {
+    "country_id":"138",
+    "code":"MX",
+    "code2":"MEX",
+    "currency_code":"MXN",
+    "name":"Mexico",
+    "mobile_prefix":"52",
+    "id":"138",
+    "icon":"https://restcountries.eu/data/mex.svg",
+    "description":"Mexico",
+    "loaded":true
+  };
+
   public loaded : boolean = false
   referrer: string = '';
   getInfo : number = 1
@@ -24,6 +38,8 @@ export class HomePage {
     Correo: new FormControl(''),
     Numero: new FormControl(''),
   });
+  countries: any;
+  flagimage: string = '';
   
 
   constructor(private trans: TranslateManagerService,private translate : TranslateService,private rootService : RootService,private kmtService:KmtService, private route : ActivatedRoute) {
@@ -35,14 +51,44 @@ export class HomePage {
 
   ionViewWillEnter(){
     this.getInfo = 1
+    this.dosearch("");
+    this.flagimage = `assets/country-flags/${this.selectedcountry.code.toLowerCase()}.png`
   }
+
+
+  dosearch(searchkey:string){
+    // console.log(searchkey)
+    if(searchkey){
+      const searched = this.kmtService.countries.filter((a: any)=>{
+        return a.name.toLowerCase().indexOf(searchkey.toLowerCase()) >-1;
+      });
+      this.countries = searched;
+    }else this.countries = this.kmtService.countries;
+    console.log(this.countries)
+  }
+
+  searchbar(event: any){
+    this.dosearch(event.target.value)
+  }
+
+
+  selectcountry(selected:any){
+    this.selectedcountry = selected
+    this.flagimage = `assets/country-flags/${this.selectedcountry.code.toLowerCase()}.png`
+    this.setOpen(false)
+    console.log(this.selectedcountry)
+  }
+  geticon(data: any) {
+    return `assets/country-flags/${data.code.toLowerCase()}.png`;
+  }
+
 
   confirm(){
     this.loaded = true
     var fname = this.profileForm.controls.Nombre.value
     var lname = this.profileForm.controls.Apellido.value
     var email = this.profileForm.controls.Correo.value
-    var number = this.profileForm.controls.Numero.value
+    var number ='+'+ this.selectedcountry.mobile_prefix + this.profileForm.controls.Numero.value
     this.kmtService.CreateInvitationCode({
       FirstName: fname,
         LastName : lname,
@@ -68,6 +114,8 @@ export class HomePage {
 
 
   setOpen(isOpen: boolean) {
+    if(isOpen == true)this.dosearch("");
     this.isModalOpen = isOpen;
+
   }
 }
